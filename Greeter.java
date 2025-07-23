0427446e08a366c3e11339bd85c3e0962feda4c1
Samantha Quilat <smquilat@gmail.com>
2025-07-23 17:43:05 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,12 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+
+    if (args.length < 3) {
+      System.out.println();
+      return;
+    }
+
     String language = args[0];
     String country = args[1];
     String name = args[2];
